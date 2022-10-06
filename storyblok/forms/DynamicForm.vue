4490f9d64dad06c4e545243c1c2b19284e3525ba
michael-mb <mbonimichael@yahoo.fr>
2022-10-07 00:10:08 +0200

    update

--- storyblok/forms/DynamicForm.vue
+++ storyblok/forms/DynamicForm.vue
@@ -7,7 +7,6 @@
         </div>
       </div>
 
-      {{contactForm}}
       <form :action="blok.endpoint" method="post" :id="blok._uid" @submit="formSubmit">
       <div class="row blog-entries">
         <div class="col-md-12 col-lg-8 main-content">
@@ -15,18 +14,19 @@
           <div class="row" v-for="input in blok.inputs" :key="input._uid" v-editable="input">
             <div v-if="input.type === 'text'" class="col-md-12 form-group">
               <label :for="input.label">{{input.label}}</label>
-              <input :id="input.name" :name="input.name" class="form-control" :type="input.text" :placeholder="input.placeholder" v-model="contactForm[input.name]">
+              <input :id="input.name" :name="input.name" class="form-control" :type="input.text"
+                     :placeholder="input.placeholder" :required="contains(input.validators, 'required')"
+                     v-model="contactForm[input.name]">
             </div>
 
-            <div v-if="input.type === 'number' && input.name !== 'age'" class="col-md-12 form-group" v-editable="input">
+            <div v-if="input.type === 'number'" class="col-md-12 form-group" v-editable="input">
               <label :for="input.label">{{input.label}}</label>
-              <input :id="input.name"  :name="input.name" class="form-control" type="number"  v-model="contactForm[input.name]" :placeholder="input.placeholder">
-            </div>
-
-            <div v-if="input.type === 'number' && input.name === 'age'" class="col-md-12 form-group" v-editable="input">
-              <label :for="input.label">{{input.label}}</label>
-              <input :id="input.name"  :name="input.name" class="form-control" type="number"  v-model="contactForm[input.name]" :placeholder="input.placeholder">
-              <p v-if="!validAge(contactForm.age)" class="error">⛔️ {{input.validators[0].errorMessage}}</p>
+              <input :id="input.name"  :name="input.name" class="form-control" type="number"
+                     v-model="contactForm[input.name]" :placeholder="input.placeholder"
+                     :required="contains(input.validators, 'required')">
+              <div v-if="contains(input.validators, 'age')">
+                <p v-if="!validAge(contactForm[input.name])" class="error">{{input.validators[0].errorMessage}}</p>
+              </div>
             </div>
 
             <div v-if="input.type === 'email'" class="col-md-12 form-group" v-editable="input">
@@ -75,6 +75,15 @@ function validEmail(email){
 
 function validAge(age){
   return age >= 18
+}
+
+function contains(validators, validator){
+  if(validators.length <= 0)
+    return false
+
+  return validators.some( elem => {
+    return elem.component === validator
+  })
 }
 
 function formSubmit(e){
